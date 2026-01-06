9e9c325b9dc2539ac3b1836db6300e74d298033b
Tristan Brideweser <tbridewe@purdue.edu>
2026-01-06 13:39:19 -0500

    creating basic tui outline

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,39 @@
+use std::{io, sync::mpsc, thread, time::Duration};
+
+use crossterm::event::{KeyCode, KeyEventKind};
+use ratatui::{
+    layout::{Constraint, Layout},
+    prelude::{Buffer, Rect},
+    style::{Color, Style, Stylize},
+    symbols::border,
+    text::Line,
+    widgets::{Block, Gauge, Widget},
+    DefaultTerminal, Frame,
+};
+
 pub struct App {
     exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(widget, frame.area())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where 
+        Self: Sized, 
+        {
+            Line::from("Process overview").bold().render(area, buf);
+        }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io;
 use app::App;
 
 fn main() -> io::Result<()> {
-    let mut terminal: Terminal<CrosstermBackend<>> = ratatui::init();
+    let mut terminal = ratatui::init();
     
     let mut app: App = App::new();
 
     let app_result = app.run(&mut terminal);
     
-    ratatui.restore();
+    ratatui::restore();
     app.result
 }
